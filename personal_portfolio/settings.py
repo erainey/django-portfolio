49bf08519d0a3262fc9cf7f16035a228a6fe8c66
Eric <dev@ericrainey.com>
2021-12-06 09:57:00 -0500

    Removes production key

--- personal_portfolio/settings.py
+++ personal_portfolio/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-#gv*(m1l=-x-5=6o4()qg4oamlktxk=i*xg74e-_)72xzzj)=*'
+# SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
